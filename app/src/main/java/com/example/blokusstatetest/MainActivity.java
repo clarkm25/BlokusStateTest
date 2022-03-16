40ed5d7ed538e30ea56e6dd3ab853d169eb6e11d
clarkm25 <clarkm25@up.edu>
2022-03-15 18:30:27 -0700

    Button now displays toStrings for the two copies made

--- app/src/main/java/com/example/blokusstatetest/MainActivity.java
+++ app/src/main/java/com/example/blokusstatetest/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Calls onClick method below
         Button runTest = findViewById(R.id.runButton);
         runTest.setOnClickListener(this);
     }
@@ -37,15 +38,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     @Override
     public void onClick (View view){
+        //Creates an EditText to display the BlokusGameState data
         EditText textDisplay = findViewById(R.id.multiTextView);
-        textDisplay.setText("");
+        textDisplay.setText(""); //Clears the display
+
+        //String that gets appended to display the GameState in the multi text.
+        String gameStateData = "";
 
         //Path for a specific piece needed for rotation
         Path firstPiece = new Path();
 
+        //Creates instances of default and copy constructor
         BlokusGameState firstInstance = new BlokusGameState();
         BlokusGameState firstCopy = new BlokusGameState(firstInstance);
 
+        //Calls all methods within BlokusGameState
         firstInstance.rotatePiece(1, firstPiece, 15, 15);
         firstInstance.placePiece(1, 10, 10, 1);
         firstInstance.helpMenu();
@@ -54,9 +61,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         BlokusGameState secondInstance = new BlokusGameState();
         BlokusGameState secondCopy = new BlokusGameState(secondInstance);
 
-        firstCopy.toString();
+        gameStateData += firstCopy.toString();
+        gameStateData +="\n ---------- \n"; //Separates first copy from second copy
 
-        secondCopy.toString();
+        gameStateData += secondCopy.toString();
 
+        //Checks for any errors or differences between copy instances
+        if (firstCopy.toString().equals(secondCopy.toString())) {
+            gameStateData += "\n The two copies are equal to each other!";
+        }
+        else {
+            gameStateData += "\n The two copies are not equal to each other..";
+        }
+
+        textDisplay.setText(gameStateData);
     }
 }
