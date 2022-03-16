662e123b572a26de60889b4e220a245a58e452d5
skyelarCann <cann24@up.edu>
2022-03-15 23:02:45 -0700

    Added editText functionality to update itself accordingly

--- app/src/main/java/com/example/blokusstatetest/MainActivity.java
+++ app/src/main/java/com/example/blokusstatetest/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Calls onClick method below
+        /* Initializes the run test button and sets the appropriate listener */
         Button runTest = findViewById(R.id.runButton);
         runTest.setOnClickListener(this);
     }
@@ -38,31 +38,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     @Override
     public void onClick (View view){
-        //Creates an EditText to display the BlokusGameState data
+        /* Creates an EditText to display the BlokusGameState data */
         EditText textDisplay = findViewById(R.id.multiTextView);
         textDisplay.setText(""); //Clears the display
 
-        //String that gets appended to display the GameState in the multi text.
+        /* String that gets appended to display the GameState in the multi text. */
         String gameStateData = "";
 
-        //Path for a specific piece needed for rotation
+        /* Path for a specific piece needed for rotation */
         Path firstPiece = new Path();
 
-        //Creates instances of default and copy constructor
+        /* Creates instances of default and copy constructor */
         BlokusGameState firstInstance = new BlokusGameState();
         BlokusGameState firstCopy = new BlokusGameState(firstInstance);
 
-        //Calls all methods within BlokusGameState
-        firstInstance.rotatePiece(1, firstPiece, 15, 15);
-        firstInstance.placePiece(1, 10, 10, 1);
+        /**
+         * Calls all methods within BlokusGameState and updates the textDisplay
+         * accordingly after each step
+         */
+        firstInstance.rotatePiece(0, firstPiece, 15, 15);
+        gameStateData += "Player one has rotated their first piece!\n";
+        textDisplay.setText(gameStateData);
+        firstInstance.placePiece(0, 10, 10, 1);
+        gameStateData += "Player one has placed a piece at the position 10,10!\n";
+        textDisplay.setText(gameStateData);
         firstInstance.helpMenu();
+        gameStateData += "The help menu has been opened!\n";
+        textDisplay.setText(gameStateData);
         firstInstance.quitGame(true);
+        gameStateData += "Game has been closed!\n";
+        textDisplay.setText(gameStateData);
 
+        /* Creates a second instance of the game state using the default ctor and then copies it */
         BlokusGameState secondInstance = new BlokusGameState();
         BlokusGameState secondCopy = new BlokusGameState(secondInstance);
 
+        /* Calls the toString method on both objects and separates them with a line */
         gameStateData += firstCopy.toString();
-        gameStateData +="\n ---------- \n"; //Separates first copy from second copy
+        gameStateData +="\n ---------- \n";
 
         gameStateData += secondCopy.toString();
 
